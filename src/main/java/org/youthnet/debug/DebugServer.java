770c7198ca7fe112b2a377aad8cb5282f00cecff
Karl Bennett <karl@karl-youthnet.(none)>
2010-05-20 17:49:28 +0100

    Duplicated config files for one-jar though it is looking like it will not be able to be used.

--- src/main/java/org/youthnet/debug/DebugServer.java
+++ src/main/java/org/youthnet/debug/DebugServer.java
@@ -8,9 +8,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Date: 18-May-2010
  */
 public class DebugServer {
-    private static AbstractApplicationContext context;
 
     public static void main(String[] args) {
-        context = new ClassPathXmlApplicationContext("config/spring/server-config.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("config/spring/server-config.xml");
     }
 }
