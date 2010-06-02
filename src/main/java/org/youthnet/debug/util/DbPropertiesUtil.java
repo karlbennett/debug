47eb29b03a57f4c8c4817371494f7423ca3dd04d
Karl Bennett <karl@karl-youthnet.(none)>
2010-06-03 00:15:26 +0100

    Created a property bean to hold all the information about the database configuration.

---
+++ src/main/java/org/youthnet/debug/util/DbPropertiesUtil.java
@@ -0,0 +1,108 @@
+package org.youthnet.debug.util;
+
+/**
+ * User: karl
+ * Date: 02-Jun-2010
+ */
+public class DbPropertiesUtil {
+
+    private String domain;
+    private String port;
+    private String sid;
+    private String url;
+    private String driver;
+    private String username;
+    private String password;
+    private String schema;
+    private String adminUsername;
+    private String adminPassword;
+    private String adminSchema;
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+
+    public String getAdminUsername() {
+        return adminUsername;
+    }
+
+    public void setAdminUsername(String adminUsername) {
+        this.adminUsername = adminUsername;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    public String getAdminSchema() {
+        return adminSchema;
+    }
+
+    public void setAdminSchema(String adminSchema) {
+        this.adminSchema = adminSchema;
+    }
+}
